eaf33de447360df7789cca43f13b7097bee5db7c
Prateek Shah <pcshah1996@gmail.com>
2016-09-16 20:15:02 -0400

    testing SQL

--- server.js
+++ server.js
@@ -32,25 +32,39 @@ app.set('view engine', 'pug');
 app.use('/og', express.static(__dirname + '/static/og.html'));
 
 app.get('/', function (req, res) {
-    for (x = 0; x < 100; x++) {
-        connection.query('INSERT INTO objects VALUES from reviews', function(err, rows, fields) {
-            if (err) throw err;
-            console.log(rows);
-        });
-        connection.query('INSERT INTO reviews VALUES from reviews', function(err, rows, fields) {
-            if (err) throw err;
-            console.log(rows);
-        });
-    }
-    connection.query('SELECT * from reviews', function(err, rows, fields) {
-        if (err) throw err;
-        console.log('Select: ', rows);
-    });
     // TODO: add variable that we can iterate through to create views
     // TODO: constant search updates so the list shrinks when typing searches
     res.render('index', { title: 'Tipsy Reviews', message: 'Hi there!'});
 });
 
+app.get('/populate1/:num', function(req, res) {
+    for (x = 0; x < req.params.id; x++) {
+        connection.query('INSERT INTO objects VALUES ...', function(err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+        });
+    }
+});
+
+app.get('/populate2/:num', function(req, res) {
+    for (x = 0; x < req.params.id; x++) {
+        connection.query('INSERT INTO reviews VALUES ...', function(err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+        });
+    }
+});
+app.get('/view', function(req, res) {
+    connection.query('SELECT * from reviews', function(err, rows, fields) {
+        if (err) throw err;
+        console.log('Select: ', rows);
+    });
+});
+
+connection.query('SELECT * from reviews', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Select: ', rows);
+});
 // For reviews
 app.get('/submit', function (req, res) {
     res.render('submission', { });
